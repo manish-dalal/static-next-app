509c457efbc31d5266eb5502ebc353eb7f5437de
Manish <manish.dalal@moonshinetechnology.com>
2023-05-03 16:07:45 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/term-and-condition': { page: '/term-and-condition' },
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+// import Image from 'next/image'
+// import { Inter } from 'next/font/google'
+import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main} `}>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
@@ -27,7 +27,7 @@ export default function Home() {
               rel="noopener noreferrer"
             >
               By{' '}
-              <Image
+              <img
                 src="/vercel.svg"
                 alt="Vercel Logo"
                 className={styles.vercelLogo}
@@ -40,7 +40,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Image
+          <img
             className={styles.logo}
             src="/next.svg"
             alt="Next.js Logo"
